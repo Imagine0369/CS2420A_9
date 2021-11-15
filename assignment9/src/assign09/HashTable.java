b54754e0c03b0ae4edc9dd78474962dc00134ebb
mikephelps <mikephelps@mikes-mbp-3.uconnect.utah.edu>
2021-11-14 17:26:04 -0700

    Initial Commit

---
+++ assignment9/src/assign09/HashTable.java
@@ -0,0 +1,152 @@
+package assign09;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class HashTable <K, V> implements Map<K, V> {
+
+	private final double LOADLIMIT = 10.0;
+	private ArrayList<LinkedList<MapEntry<K, V>>> table;
+	private int size;
+	private int capacity;
+	private double loadFactor = size / capacity;
+	
+//	public HashTable(int capacity) {
+//		size = 0;
+//		this.capacity = capacity;
+//		table = new ArrayList<LinkedList<MapEntry<K, V>>>();
+//		loadFactor = size/capacity;
+//		//Fill AList with initial capacity of linked lists
+//		for(int i = 0; i < capacity; i++)
+//		   table.add(new LinkedList<MapEntry<K, V>>());
+//		
+//		
+//	}
+	
+	public HashTable() {
+		size = 0;
+		capacity = 100;
+		table = new ArrayList<LinkedList<MapEntry<K, V>>>();
+		loadFactor = size/capacity;
+		//Fill AList with initial capacity of linked lists
+		for(int i = 0; i < capacity; i++) {
+		   table.add(new LinkedList<MapEntry<K, V>>());
+		}
+	}
+	
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean containsKey(K key) {
+		//Iterate through the map entries inside the linked list associated with the appropriate index value
+		for(MapEntry<K, V> item : table.get(key.hashCode() % capacity)) {
+			//If key value within MapEntry item equals key we are looking for, return true
+			if(item.getKey().equals(key)){
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
+	@Override
+	public boolean containsValue(V value) {
+		//Iterate through each item in each linked list to find if
+		//value is found in any MapEntry
+		for(LinkedList<MapEntry<K, V>> list : table) {
+			for(MapEntry<K, V> entry : list) {
+				if(value.equals(entry.getValue())) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+
+	@Override
+	public List<MapEntry<K, V>> entries() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V get(K key) {
+		for(MapEntry <K, V> entry:table.get(key.hashCode() % capacity)) {
+			//If entry key value equals key, return entry value item
+			if(entry.getKey().equals(key)) {
+				return entry.getValue();
+			}
+		}
+		
+		//If map does not have key, return null
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return size == 0;
+	}
+
+	@Override
+	public V put(K key, V value) {
+		//Iterate through LinkedList at appropriate index
+		for(MapEntry <K, V> entry:table.get(key.hashCode() % capacity)) {
+		//If entry key value already exists, simply updates its value
+			if(entry.getKey().equals(key)) {
+				V returnVal = entry.getValue();
+				entry.setValue(value);
+				return returnVal;
+			}
+		}
+		
+		//If key does not already exist, add map entry to end of LinkedList
+		//grow and re-hash if necessary.
+		
+		if(((size + 1) / capacity) >= LOADLIMIT) {
+			growAndRehash();
+		}
+		
+		//Create MapEntry using values asked to be put into table
+		MapEntry<K, V> entry = new MapEntry<K, V>(key, value);
+		
+		//Put entry into the LinkedList at appropriate index value
+		//Appropriate index value is determined by keys hashcode mod capacity
+		//table is ArrayList
+		table.get(key.hashCode() % capacity).add(entry);
+		size++;
+		
+		return null;
+	}
+	
+	private void growAndRehash() {
+		ArrayList<LinkedList<MapEntry<K, V>>> grown = new ArrayList<LinkedList<MapEntry<K, V>>>();
+		
+		//New ArrayList double the size of the previous AList
+		for(int i = 0; i < capacity*2; i++) {
+			   table.add(new LinkedList<MapEntry<K, V>>());
+			}
+		
+		//for()
+	}
+
+	@Override
+	public V remove(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+}
+
+
